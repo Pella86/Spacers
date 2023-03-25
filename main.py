7b8f1107e5c052f82ad7b9e0b9cee8d883df37d4
pella <pella@home.here>
2023-03-25 13:41:12 +0100

    spaceship moves and stays inside borders

--- main.py
+++ main.py
@@ -81,26 +81,141 @@ def test_planet(screen):
     planet_2 = Planet(planet_radius, planet_position * 2, planet_color)
     
     planet_2.blit(screen)
+
+
+class Spaceship(pygame.sprite.Sprite):
+
+    def __init__(self):
+        super().__init__()
+        
+        self.radius = 50
+        self.center = pygame.Vector2(self.radius, self.radius)
+        self.size = pygame.Vector2(self.radius*2, self.radius*2)
+        color = "blue"
+        
+        self.direction = pygame.Vector2(0, 1)
+        
+        position = pygame.Vector2(SCREEN_SIZE) / 2
+        
+        # accelleration
+        self.thrust_force = 1
+        self.force = 0
+        
+        # image stuff
+        self.image = pygame.Surface(self.size, pygame.SRCALPHA)
+        
+        pygame.draw.circle(self.image, color, self.center, self.radius)
+        
+        self.original_image = self.image.copy()
+        
+        self.image.fill((10, 0, 0, 10))
+        
+        self.rect = self.image.get_rect(center=position)
+        
+        self.draw_dir()
+    
+    
+    def draw_dir(self):
+        self.image.blit(self.original_image, self.original_image.get_rect(topleft=(0, 0)))
+        
+        dir_image = pygame.Surface(self.size, pygame.SRCALPHA)
+        start_pos = self.center
+        end_pos = self.center + self.direction * (self.radius * (1 - 0.01))
+        pygame.draw.line(dir_image, "green", start_pos, end_pos)
+        
+        dir_rect = dir_image.get_rect(topleft=(0,0))
+
+        self.image.blit(dir_image, dir_rect)  
+        
+    def borders(self, screen):
+        print(self.rect.y, self.rect.y > screen.get_height())
+        
+        pos = pygame.Vector2(self.rect.center)
+        
+        if pos.x - self.radius < 0:
+            self.rect.center = (self.radius, pos.y)
+            self.force = 0
+        
+        if pos.x > screen.get_width() - self.radius:
+            self.rect.center =  (screen.get_width() - self.radius, pos.y)
+            self.force = 0
+        
+        if pos.y - self.radius < 0:
+            self.rect.center = (pos.x, self.radius)
+            self.force = 0
+         
+        if pos.y > screen.get_height() - self.radius:
+            self.rect.center = (pos.x, screen.get_height() - self.radius)
+            self.force = 0
+            
+    
+    def change_dir(self):
+        if pygame.key.get_pressed()[pygame.K_a]:
+            self.direction = self.direction.rotate(-10) 
+            self.draw_dir()
+
+        if pygame.key.get_pressed()[pygame.K_d]:
+            self.direction = self.direction.rotate(+10) 
+            self.draw_dir()  
    
+    def move(self):
+        mass = 1
+            
+        acceleration = self.force * mass
+        dt = 1
+        delta_pos = self.direction * (acceleration * (dt ** 2))
+            
+            
+        self.rect.center += delta_pos
+             
+        if pygame.key.get_pressed()[pygame.K_w]:
+            self.force += self.thrust_force
 
-
+                  
+'''
 def test_spaceship(screen):
+    sp_spaceship = pygame.sprite.Group()
+    sp_spaceship.add(Spaceship())
     
-    spaceship_size = pygame.Vector2(100, 100)
-    spaceship_color = "blue"
-    spaceship_position = pygame.Vector2(SCREEN_SIZE) / 2
+    sp_spaceship.draw(screen)
+
     
+    
+    # draw the space ship image
     spaceship_image = pygame.Surface(spaceship_size, pygame.SRCALPHA)
     spaceship_image.fill((10, 0, 0, 10))
     
     
-    pygame.draw.circle(spaceship_image, spaceship_color, spaceship_size / 2, spaceship_size[0] / 2)
-    
-    screen.blit(spaceship_image, spaceship_image.get_rect(center=spaceship_position))
-    
-    if 
+    pygame.draw.circle(spaceship_image, spaceship_color, spaceship_center, spaceship_radius)
     
 
+    
+    # draw the direction
+    dir_image = pygame.Surface(spaceship_size, pygame.SRCALPHA)
+    start_pos = spaceship_center
+    end_pos = spaceship_center + spaceship_dir * spaceship_radius
+    pygame.draw.line(dir_image, "green", start_pos, end_pos)
+    dir_rect = dir_image.get_rect(topleft=(0,0))
+    
+    spaceship_image.blit(dir_image, dir_rect)
+    
+    if pygame.key.get_pressed()[pygame.K_a]:
+        print("pressed a")        
+        
+        
+
+    screen.blit(spaceship_image, spaceship_image.get_rect(center=spaceship_position))    
+'''
+
+def test_spaceship(screen, spaceship):
+    spaceship.change_dir()
+    spaceship.move()
+    spaceship.borders(screen)
+    
+    sp_spaceship = pygame.sprite.Group()
+    sp_spaceship.add(spaceship)
+    
+    sp_spaceship.draw(screen)    
     
     
     
@@ -121,7 +236,10 @@ class GameState:
     
 
     def __init__(self):
-        self.state = self.TITLE_SCREEN
+        self.state = self.TEST_SPACESHIP
+        
+        self.spaceship = Spaceship()
+        
     
     def run(self, screen):
         if self.state == self.TITLE_SCREEN:
@@ -134,7 +252,7 @@ class GameState:
             test_planet(screen)
         
         if self.state == self.TEST_SPACESHIP:
-            test_spaceship(screen)
+            test_spaceship(screen, self.spaceship)
             
 
 def main():
