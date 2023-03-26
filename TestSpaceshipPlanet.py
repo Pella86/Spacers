6654f01ba06dab6437cf17a9f2b3b99c31944b00
pella <pella@home.here>
2023-03-26 12:33:30 +0200

    Added spaceship and planets test

---
+++ TestSpaceshipPlanet.py
@@ -0,0 +1,77 @@
+import pygame
+
+import Spaceship
+import Planet
+
+class TestSpaceshipPlanet:
+
+    def __init__(self, screen):
+        self.screen = screen
+        
+        initial_position = pygame.Vector2(screen.get_size()) / 2
+        self.spaceship = Spaceship.Spaceship(initial_position)
+        
+        self.planet_list = []
+        
+        self.planet_list.append(Planet.Planet(100, (320, 110), (255, 255, 0)))
+        self.planet_list.append(Planet.Planet(200, (800, 400), (255, 0, 255)))   
+        
+        self.sp_planets = pygame.sprite.Group()
+        for planet in self.planet_list:
+            self.sp_planets.add(planet)
+
+        self.sp_spaceship = pygame.sprite.Group()
+        self.sp_spaceship.add(self.spaceship)     
+    
+    
+    def run(self):
+ 
+        if pygame.sprite.spritecollide(self.spaceship, self.sp_planets, False):
+            print("Rectangle collision detected")
+            
+            planets = pygame.sprite.spritecollide(self.spaceship, self.sp_planets, False, pygame.sprite.collide_mask)
+            
+            if planets:
+                print("sprite collision")
+                for planet in planets:
+                    
+                    # take the position of the planet
+                    planet_pos = planet.get_pos()
+                    
+                    # take the position of the spaceship
+                    spaceship_pos = self.spaceship.get_pos()
+                    
+                    # calculate the vector
+                    outward_vec = spaceship_pos - planet_pos
+                    
+                    if outward_vec == pygame.Vector2(0, 0):
+                        outward_vec = pygame.Vector(1, 0)
+                    
+                    
+                    outward_vec.normalize_ip()
+                    
+                    
+                    self.spaceship.rect.center = planet_pos + outward_vec * (self.spaceship.radius + planet.radius + 2)
+                    
+                    self.spaceship.reset_force()
+                    
+                    print("moving space ship to:", self.spaceship.rect.center)
+                    
+                    
+                    
+                    
+        self.spaceship.change_dir()
+        
+        self.spaceship.calc_forces(self.planet_list)
+        self.spaceship.move()
+        self.spaceship.borders(self.screen)            
+            
+        
+        # draw planets
+        self.sp_planets.draw(self.screen)
+        
+        # draw the spaceship
+        self.sp_spaceship.draw(self.screen) 
+        
+        
+               
--- main.py
+++ main.py
@@ -3,77 +3,10 @@ import pygame
 import TitleScene
 import TestPlanet
 import TestSpaceship
+import TestSpaceshipPlanet
+
 
 SCREEN_SIZE = (1240, 640)
-    
-
-def test_spaceship_planet_init(game_state):
-    game_state.spaceship = Spaceship()
-    
-    game_state.planet_list = []
-    
-    game_state.planet_list.append(Planet(100, (320, 110), (255, 255, 0)))
-    game_state.planet_list.append(Planet(200, (800, 400), (255, 0, 255)))
-    game_state.sp_planets = pygame.sprite.Group()
-    for planet in game_state.planet_list:
-        game_state.sp_planets.add(planet)
-
-    game_state.sp_spaceship = pygame.sprite.Group()
-    game_state.sp_spaceship.add(game_state.spaceship)
-
-def test_spaceship_planet(screen, game_state):
-
-    if pygame.sprite.spritecollide(game_state.spaceship, game_state.sp_planets, False):
-        print("collision detected")
-        
-        spaceship = game_state.spaceship
-        planets = pygame.sprite.spritecollide(spaceship, game_state.sp_planets, False, pygame.sprite.collide_mask)
-        if planets:
-            print("sprite collision")
-            for planet in planets:
-                
-                # take the position of the planet
-                planet_pos = planet.get_pos()
-                
-                # take the position of the spaceship
-                spaceship_pos = spaceship.get_pos()
-                
-                # calculate the vector
-                outward_vec = spaceship_pos - planet_pos
-                
-                if outward_vec == pygame.Vector2(0, 0):
-                    outward_vec = pygame.Vector(1, 0)
-                
-                
-                outward_vec.normalize_ip()
-                
-                
-                spaceship.rect.center = planet_pos + outward_vec * (spaceship.radius + planet.radius + 2)
-                
-                spaceship.reset_force()
-                
-                print("moving space ship to:", game_state.spaceship.rect.center)
-                
-                
-                
-                
-    game_state.spaceship.change_dir()
-    
-    game_state.spaceship.calc_forces(game_state.planet_list)
-    game_state.spaceship.move()
-    game_state.spaceship.borders(screen)            
-        
-    
-    # draw planets
-    game_state.sp_planets.draw(screen)
-    
-    # draw the spaceship
-    game_state.sp_spaceship.draw(screen)            
-    
-    
-    
-    
-    
 
 
 class GameState:
@@ -89,7 +22,7 @@ class GameState:
     
 
     def __init__(self, screen):
-        self.state = self.TEST_SPACESHIP
+        self.state = self.TEST_SPACEPLANET
         
         if self.state == self.TITLE_SCREEN:
             self.title_screen = TitleScene.TitleScene(screen)
@@ -101,7 +34,7 @@ class GameState:
             self.test_spaceship = TestSpaceship.TestSpaceship(screen)
         
         if self.state == self.TEST_SPACEPLANET:
-            test_spaceship_planet_init(self)
+            self.scene = TestSpaceshipPlanet.TestSpaceshipPlanet(screen)
         
     
     def run(self, screen):
@@ -118,7 +51,7 @@ class GameState:
             self.test_spaceship.run()
         
         if self.state == self.TEST_SPACEPLANET:
-            test_spaceship_planet(screen, self)
+            self.scene.run()
             
 
 def main():
